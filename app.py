bd7ec62fa7bcf68b40fa2f5b9fdcd3c8c4e7865d
Afinch97 <49850011+Afinch97@users.noreply.github.com>
2022-02-07 17:07:55 -0500

    Almost done

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def hello_world():
+    data = get_trending()
     
     return render_template(
         "index.html",
-        
+        titles = data['titles'],
+        overviews = data['overviews'],
+        posters = data['posters'],
         )
+    
+app.run(
+    host=os.getenv('IP','0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug = True
+)
--- tmdb.py
+++ tmdb.py
@@ -1,10 +1,33 @@
+import os
+
 import requests
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week?api_key='+ os.getenv('TMBD_KEY')
+POSTER_PATH = 'https://image.tmdb.org/t/p/w185'
 
 def get_trending():
     response = requests.get(BASE_URL)
-    return response
+    data = response.json()
+    movies = data['results']
+    
+    def get_title(movies):
+        return movies['title']
+    
+    def get_poster(movies):
+        return (POSTER_PATH +movies['poster_path'])
+    
+    def get_ov(movies):
+        return movies['overview']
+    
+    titles = map(get_title, movies)
+    posters = map(get_poster, movies)
+    overviews = map(get_ov, movies)
+    
+    return {
+        'titles': list(titles),
+        'overviews': list(overviews),
+        'posters': list(posters),
+    }
